38be3a088761841d98c2dc51f35f5dae19691223
jonatasDantas <jonatas.almeida14@hotmail.com>
2020-09-21 11:09:30 -0300

    Validation config adviser and fix on project dependencies

---
+++ src/main/java/br/com/jowdev/projetosiplataformaempregos/config/validation/ErrorFormDto.java
@@ -0,0 +1,19 @@
+package br.com.jowdev.projetosiplataformaempregos.config.validation;
+
+public class ErrorFormDto {
+	private String field;
+	private String error;
+
+	public ErrorFormDto(String field, String error) {
+		this.field = field;
+		this.error = error;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public String getError() {
+		return error;
+	}
+}
